7aa9b5ec687710edc1ab35349e852de0162dbaac
Elijah Noble <elijahnobledev@gmail.com>
2022-03-19 19:58:04 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+    console.log(cats);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    console.log(cats);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    console.log(cats);
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    console.log(cats);
+}
+
+function appendCat(name){    
+    console.log([...cats, name]);
+    return [...cats, name]
+}
+
+function prependCat(name){
+    console.log(name, [...cats]);
+    return [name, ...cats]
+}
+
+function removeLastCat(){
+    console.log(cats.slice(0, 2))
+    return cats.slice(0, 2)
+}
+
+function removeFirstCat(){
+    console.log(cats.slice(1))
+    return cats.slice(1)
+}
